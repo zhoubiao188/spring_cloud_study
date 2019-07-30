40bc5315ec5c36d06ab14ddac03c34e1a5bfcd63
zhoubiao <zhoubiao@zhoubiaodeMacBook-Pro.local>
2019-07-30 10:29:17 +0800

    RestTemplate的用法讲解

---
+++ RestTemplate/consumer/src/main/java/cn/com/scitc/consumer/ConsumerApplication.java
@@ -0,0 +1,19 @@
+package cn.com.scitc.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
