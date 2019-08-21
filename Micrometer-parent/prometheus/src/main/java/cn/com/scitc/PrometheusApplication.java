340484f640fecf31a54b4a78d1d666f05fc0a6e3
zhoubiao188 <zz863146675bb@icloud.com>
2019-08-21 18:08:46 +0800

    Prometheus用法

---
+++ Micrometer-parent/prometheus/src/main/java/cn/com/scitc/PrometheusApplication.java
@@ -0,0 +1,13 @@
+package cn.com.scitc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrometheusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( PrometheusApplication.class, args );
+    }
+
+}
