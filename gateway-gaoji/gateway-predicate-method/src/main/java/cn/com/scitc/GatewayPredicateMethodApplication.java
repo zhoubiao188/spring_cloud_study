0271a2c6de3f98041436cdb0402458568ff6de08
zhoubiao188 <zz863146675bb@icloud.com>
2019-09-13 18:04:01 +0800

    请求方式转发

---
+++ gateway-gaoji/gateway-predicate-method/src/main/java/cn/com/scitc/GatewayPredicateMethodApplication.java
@@ -0,0 +1,13 @@
+package cn.com.scitc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPredicateMethodApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( GatewayPredicateMethodApplication.class, args );
+    }
+
+}
