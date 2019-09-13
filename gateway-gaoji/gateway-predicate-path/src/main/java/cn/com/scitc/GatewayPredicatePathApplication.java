b4663eac13b780a86c15a7724ad25cbff849602f
zhoubiao188 <zz863146675bb@icloud.com>
2019-09-13 18:31:40 +0800

    请求路径匹配

---
+++ gateway-gaoji/gateway-predicate-path/src/main/java/cn/com/scitc/GatewayPredicatePathApplication.java
@@ -0,0 +1,13 @@
+package cn.com.scitc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayPredicatePathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run( GatewayPredicatePathApplication.class, args );
+    }
+
+}
