dc9792bec888fce942267c24f956597a03d27478
zhoubiao188 <zz863146675bb@icloud.com>
2019-09-14 12:27:41 +0800

    网关过滤器组合使用

--- gateway-gaoji/gateway-filter-AddRequestParameter/common/src/main/java/cn/com/scitc/UserVo.java
+++ gateway-gaoji/gateway-filter-AddRequestParameter/common/src/main/java/cn/com/scitc/UserVo.java
@@ -8,4 +8,40 @@
 package cn.com.scitc;
 
 public class UserVo {
+    private String nickname;
+    private String job;
+    private String address;
+    private Integer age;
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
