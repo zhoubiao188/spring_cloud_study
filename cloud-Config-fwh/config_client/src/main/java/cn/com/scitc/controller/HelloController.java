0b5ddbcabcae47d6229cca15b9f16b5e28848be3
zhoubiao188 <zz863146675bb@icloud.com>
2019-10-07 21:33:50 +0800

    远端配置文件

---
+++ cloud-Config-fwh/config_client/src/main/java/cn/com/scitc/controller/HelloController.java
@@ -0,0 +1,11 @@
+/**
+ * @program cloud-Config-fwh
+ * @description:
+ * @author: zhoubiao
+ * @create: 2019/10/07 19:07
+ */
+
+package cn.com.scitc.controller;
+
+public class HelloController {
+}
