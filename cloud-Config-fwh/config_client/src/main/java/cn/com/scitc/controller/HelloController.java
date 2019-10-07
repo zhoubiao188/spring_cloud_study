fb625f0c10775d328ea5f7580c127cc75ae94036
zhoubiao188 <zz863146675bb@icloud.com>
2019-10-07 21:34:55 +0800

    Config Client

--- cloud-Config-fwh/config_client/src/main/java/cn/com/scitc/controller/HelloController.java
+++ cloud-Config-fwh/config_client/src/main/java/cn/com/scitc/controller/HelloController.java
@@ -7,5 +7,19 @@
 
 package cn.com.scitc.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
 public class HelloController {
+    @Value("${love}")
+    String love;
+
+    @GetMapping("/love")
+    public String name() {
+        return love;
+    }
 }
