e62b9d4d0f65db5c4cf58f421794cc889870d489
zhoubiao188 <zz863146675bb@icloud.com>
2019-10-07 16:07:51 +0800

    读取远端配置客户端

--- cloud-config-salt/cloudConfig/config_client/src/main/java/cn/com/scitc/HelloController.java
+++ cloud-config-salt/cloudConfig/config_client/src/main/java/cn/com/scitc/HelloController.java
@@ -7,5 +7,25 @@
 
 package cn.com.scitc;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    @Value("${love}")
+    String love;
+
+    @Value("${name}")
+    String name;
+
+    @GetMapping("/hello")
+    public String hello() {
+        return love;
+    }
+
+    @GetMapping("/name")
+    public String name() {
+        return name;
+    }
 }
